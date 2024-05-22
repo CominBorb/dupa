3f70866704ad76659f01995066eed2c720e389bb
unknown <norbert.oledzki@gmail.com>
2024-05-22 15:56:31 +0200

    ok

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+    public static void main(String[] args){
+
+        double x = Pierwszpp;
+        double y = Drugapp;
+
+        System.in.printL()
+
+        double z = Math.max(x, y);
+
+
+        System.out.println(z);
+    }
+}
